d1d448e117f1f27cffec7a5af26828bb37cbaba2
Juan Carlos Guzman Rosales <jc@photoshelter.com>
2025-08-22 11:53:58 -0600

    Initial Commit

---
+++ TowerOfHanoi/TowerOfHanoiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowerOfHanoiApp.swift
+//  TowerOfHanoi
+//
+//  Created by Juan Carlos Guzman Rosales on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TowerOfHanoiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
