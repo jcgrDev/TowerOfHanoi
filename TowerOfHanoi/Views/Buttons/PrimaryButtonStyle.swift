2f0081230cdf008ed7f199cb9e99667902a83d0b
Juan Carlos Guzman Rosales <jc@photoshelter.com>
2025-08-26 21:14:16 -0600

    add button styles

---
+++ TowerOfHanoi/Views/Buttons/PrimaryButtonStyle.swift
@@ -0,0 +1,21 @@
+//
+//  PrimaryButtonStyle.swift
+//  TowerOfHanoi
+//
+//  Created by Juan Carlos Guzman Rosales on 26/08/25.
+//
+
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
+            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+    }
+}
